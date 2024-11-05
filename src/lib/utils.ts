b3ee5f34a5e6f6be2e5d219df4c84fe83499dfd9
2DC <kkts9308@gmail.com>
2024-11-05 23:50:48 +0900

    feat: add getRandomHexColorCode

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,3 +5,8 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getRandomHexColorCode() {
+  const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`
+  return randomColor
+}
