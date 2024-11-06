e7384ca539c1b743cb054e095ddb0038064931dd
2DC <kkts9308@gmail.com>
2024-11-06 23:10:26 +0900

    config: update eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -31,6 +31,7 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true }
       ],
+      '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-empty-object-type': 'off',
       'prettier/prettier': 'error'
     }
