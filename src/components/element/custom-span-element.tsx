d55f5eeedf5d5f6451f3c5d76329c3d17dc079f8
2DC <kkts9308@gmail.com>
2024-11-06 23:14:03 +0900

    feat: add Drag feature at custom div, p, span

--- src/components/element/custom-span-element.tsx
+++ src/components/element/custom-span-element.tsx
@@ -1,27 +1,87 @@
-import { FunctionComponent, HTMLAttributes } from 'react'
+import { FunctionComponent, HTMLAttributes, useRef } from 'react'
 import { cn } from '@/lib/utils'
+import { useDrag, useDrop } from 'react-dnd'
+import { DraggableCustomElement } from '@/types/element'
+import { Identifier } from 'dnd-core'
+import { DRAG_ITEM_TYPE } from '@/constants/element'
 
 interface CustomSpanElementProps extends HTMLAttributes<HTMLSpanElement> {
   width: number
   height: number
   bgColor: string
+  id: string
+  index: number
+  moveElement: (dragIndex: number, hoverIndex: number) => void
 }
 
 export const CustomSpanElement: FunctionComponent<CustomSpanElementProps> = ({
-  className,
+  id,
   width,
   height,
+  className,
   bgColor,
+  index,
+  moveElement,
   ...props
 }): JSX.Element => {
+  const ref = useRef<HTMLDivElement>(null)
+  const [{ handlerId }, drop] = useDrop<
+    DraggableCustomElement,
+    void,
+    { handlerId: Identifier | null }
+  >({
+    accept: DRAG_ITEM_TYPE.element,
+    collect(monitor) {
+      return {
+        handlerId: monitor.getHandlerId()
+      }
+    },
+    hover(item: DraggableCustomElement) {
+      if (!ref.current) {
+        return
+      }
+
+      const dragIndex = item.index
+      const hoverIndex = index
+      // Don't replace items with themselves
+      if (dragIndex === hoverIndex) {
+        return
+      }
+
+      moveElement(dragIndex, hoverIndex)
+
+      // Note: we're mutating the monitor item here!
+      // Generally it's better to avoid mutations,
+      // but it's good here for the sake of performance
+      // to avoid expensive index searches.
+      item.index = hoverIndex
+    }
+  })
+
+  const [{ isDragging }, drag] = useDrag({
+    type: DRAG_ITEM_TYPE.element,
+    item: () => {
+      return { id, index }
+    },
+    collect: (monitor: any) => ({
+      isDragging: monitor.isDragging()
+    })
+  })
+
+  const opacity = isDragging ? 0 : 1
+  drag(drop(ref))
+
   return (
     <span
+      ref={ref}
       className={cn(className)}
       style={{
+        opacity,
         width: width,
         height: height,
         backgroundColor: bgColor
       }}
+      data-handler-id={handlerId}
       {...props}
     >
       Span
