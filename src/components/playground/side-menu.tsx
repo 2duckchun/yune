073952c18dc0aa5e87205a55cbae73627f4451f2
2DC <kkts9308@gmail.com>
2024-11-08 00:36:47 +0900

    refact: separate inner code to individual component

--- src/components/playground/side-menu.tsx
+++ src/components/playground/side-menu.tsx
@@ -1,95 +1,89 @@
 import { FunctionComponent, HTMLAttributes } from 'react'
-import { cn } from '@/lib/utils'
 import { useElementStateManagingContext } from '@/contexts/element-state-manage-context'
 import { SideMenuElementButton } from '../element/side-menu-element-button'
+import { Button } from '@/components/shared/Button'
 
 interface SideMenuProps extends HTMLAttributes<HTMLDivElement> {}
 
-export const SideMenu: FunctionComponent<SideMenuProps> = ({
-  className,
-  ...props
-}): JSX.Element => {
-  const {
-    elementList,
-    setGlobalAlignHander,
-    addNewElement,
-    modifyGroupElementAlign
-  } = useElementStateManagingContext()
+export const SideMenu: FunctionComponent<SideMenuProps> = (): JSX.Element => {
   return (
     <aside
-      className={cn(
-        'flex min-h-screen w-[250px] shrink-0 flex-col gap-3 bg-emerald-400 text-3xl',
-        className
-      )}
-      {...props}
+      className={
+        'flex min-h-screen w-[250px] shrink-0 flex-col gap-3 bg-emerald-400 text-3xl'
+      }
     >
-      <div className="bg-gray-600 p-2">
-        <h2 className="text-center text-lg text-gray-50">Align</h2>
-        <div className="flex flex-col gap-2 p-3">
-          <button
-            className="text-sm"
-            onClick={() => setGlobalAlignHander('vertical')}
-          >
-            All Vertically
-          </button>
-          <button
-            className="text-sm"
-            onClick={() => setGlobalAlignHander('horizontal')}
-          >
-            All Horizontally
-          </button>
-          <button
-            className="text-sm"
-            onClick={() => modifyGroupElementAlign('vertical')}
-          >
-            Group Vertically
-          </button>
-          <button
-            className="text-sm"
-            onClick={() => modifyGroupElementAlign('horizontal')}
-          >
-            Group Horizontally
-          </button>
-        </div>
+      <AlignButtonComponents />
+      <AddElementButtonComponents />
+      <ElementListAtSideMenu />
+    </aside>
+  )
+}
+
+const AlignButtonComponents = () => {
+  const { setGlobalAlignHander, modifyGroupElementAlign } =
+    useElementStateManagingContext()
+
+  return (
+    <div className="bg-gray-600 p-2">
+      <h2 className="text-center text-lg text-gray-50">Align</h2>
+      <div className="flex flex-col gap-2 p-3">
+        <Button onClick={() => setGlobalAlignHander('vertical')}>
+          All Vertically
+        </Button>
+        <Button onClick={() => setGlobalAlignHander('horizontal')}>
+          All Horizontally
+        </Button>
+        <Button onClick={() => modifyGroupElementAlign('vertical')}>
+          Group Vertically
+        </Button>
+        <Button onClick={() => modifyGroupElementAlign('horizontal')}>
+          Group Horizontally
+        </Button>
       </div>
-      <div className="bg-gray-600 p-2">
-        <h2 className="text-center text-lg text-gray-50">Add</h2>
-        <div className="flex flex-col gap-2 p-3">
-          <button className="text-sm" onClick={() => addNewElement('div')}>
-            Div
-          </button>
-          <button className="text-sm" onClick={() => addNewElement('span')}>
-            Span
-          </button>
-          <button className="text-sm" onClick={() => addNewElement('p')}>
-            Paragraph
-          </button>
-        </div>
+    </div>
+  )
+}
+
+const AddElementButtonComponents = () => {
+  const { addNewElement } = useElementStateManagingContext()
+  return (
+    <div className="bg-gray-600 p-2">
+      <h2 className="text-center text-lg text-gray-50">Add</h2>
+      <div className="flex flex-col gap-2 p-3">
+        <Button onClick={() => addNewElement('div')}>Div</Button>
+        <Button onClick={() => addNewElement('span')}>Span</Button>
+        <Button onClick={() => addNewElement('p')}>Paragraph</Button>
       </div>
-      <div className="bg-gray-600 p-2">
-        <h2 className="text-center text-lg text-gray-50">List</h2>
-        <div className="flex flex-col gap-2 p-3">
-          {elementList.map((element, index) => {
-            if (element.isGroup) {
-              return (
-                <SideMenuElementButton
-                  index={index}
-                  id={element.id}
-                  isGrouped
-                  tag="group"
-                />
-              )
-            }
+    </div>
+  )
+}
+
+const ElementListAtSideMenu = () => {
+  const { elementList } = useElementStateManagingContext()
+  return (
+    <div className="bg-gray-600 p-2">
+      <h2 className="text-center text-lg text-gray-50">List</h2>
+      <div className="flex flex-col gap-2 p-3">
+        {elementList.map((element, index) => {
+          if (element.isGroup) {
             return (
               <SideMenuElementButton
                 index={index}
                 id={element.id}
-                tag={element.tag}
+                isGrouped
+                tag="group"
               />
             )
-          })}
-        </div>
+          }
+          return (
+            <SideMenuElementButton
+              index={index}
+              id={element.id}
+              tag={element.tag}
+            />
+          )
+        })}
       </div>
-    </aside>
+    </div>
   )
 }
